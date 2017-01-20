5d631b5b82432adfe5aec7d8b6ee20146d29d810
Britt Dodd <brittman914@gmail.com>
2017-01-20 11:13:47 -0500

    added scheduler for job, download_images will eventually be dockerized

--- download_images.py
+++ download_images.py
@@ -1,20 +1,29 @@
 import boto3
-
+import schedule
+import time
 bucketLocation = 'home-displayboard'
 fileLocation = '/tmp/'
 
-s3 = boto3.client('s3')
-list = s3.list_objects(Bucket=bucketLocation)['Contents']
 
-for s3_key in list:
-    s3_object = s3_key['Key']
-    if not s3_object.endswith("/"):
-        s3.download_file(bucketLocation, s3_object, fileLocation+s3_object)
-    else:
-        import os
-        if not os.path_exists(s3_object):
-            os.makedirs(s3_object)
 
+def job():
+    s3 = boto3.client('s3')
+    list = s3.list_objects(Bucket=bucketLocation)['Contents']
+
+    for s3_key in list:
+        s3_object = s3_key['Key']
+        if not s3_object.endswith("/"):
+            s3.download_file(bucketLocation, s3_object, fileLocation+s3_object)
+        else:
+            import os
+            if not os.path_exists(s3_object):
+                os.makedirs(s3_object)
+
+schedule.every(10).minutes.do(job)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
 
 
 
