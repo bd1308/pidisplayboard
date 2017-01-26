34e04f27e55d796dc225dc1581afaef6a19dfc25
Britt Dodd <bdodd@royall.com>
2017-01-26 13:31:29 -0500

    setting image timeout to 8 sec from 5

--- download_images.py
+++ download_images.py
@@ -102,7 +102,7 @@ while running:
         scope.screen.blit(img, (0, 0))
         scope.screen.blit(desc, 0, scope.screen.get_height())
         pygame.display.update()
-        time.sleep(5)
+        time.sleep(8)
     filelist.close()
 pygame.quit()
 
